e284929314fc0457412fd3e23418285540e5110e
AzizRakhim <azizbekrahimjonov80@gmail.com>
2022-02-15 23:26:07 +0500

    finished till footer

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let elSearchBox = document.getElementById("searchBox");
+let elSearchBtn = document.getElementById("searchBtn");
+
+elSearchBtn.addEventListener("click", function() {
+  elSearchBox.classList.toggle("show")
+})
+
+// 
+
